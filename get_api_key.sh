a063cdf222465978aaad58c92f33e62cdfc4ba26
hadyyu <496628851@qq.com>
2024-06-07 14:34:07 +0200

    change the quatation marks to add string

--- get_api_key.sh
+++ get_api_key.sh
@@ -1,6 +1,6 @@
 curl --location --request POST 'https://open.tiktokapis.com/v2/oauth/token/'  \
 --header 'Content-Type: application/x-www-form-urlencoded' \
 --header 'Cache-Control: no-cache' \
---data-urlencode 'client_key=${CLIENT_KEY}' \
---data-urlencode 'client_secret=${CLIENT_SECRET}' \
+--data-urlencode 'client_key='${CLIENT_KEY}'' \
+--data-urlencode 'client_secret='${CLIENT_SECRET}'' \
 --data-urlencode 'grant_type=client_credentials' 
